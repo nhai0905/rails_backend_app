d7036f30194122042ae4b4c5d3ae95edab625bb4
hai.tran <haiit0905@gmail.com>
2021-07-17 20:56:42 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,38 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
+namespace :puma do
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
+  
+    before :start, :make_dirs
+  end
+  
+  namespace :deploy do
+    desc 'Initial Deploy'
+    task :initial do
+      on roles(:app) do
+        before 'deploy:restart', 'puma:start'
+        invoke 'deploy'
+      end
+    end
+  
+    desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+    end
+    
+    after  :finishing,    :cleanup
+    after  :finishing,    :restart
+  end
+
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
