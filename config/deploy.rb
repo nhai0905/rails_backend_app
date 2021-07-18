3cc32b09ae2394ae642169834a03cb6d18f54c56
hai.tran <haiit0905@gmail.com>
2021-07-18 21:21:45 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+set :puma_restart_command, 'bundle exec --keep-file-descriptors puma'
 
 
 # Default value for :format is :airbrussh.
--- config/puma.rb
+++ config/puma.rb
@@ -6,6 +6,7 @@ threads 1, 6
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
+
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
