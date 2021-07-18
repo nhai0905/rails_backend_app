aa31aee8a3e495a3e917cb12a721a1f992db9624
hai.tran <haiit0905@gmail.com>
2021-07-18 21:10:54 +0700

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,25 +8,9 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :stage, :production
 set :rails_env, :production
-set :deploy_to, "/home/ngochai/projects/rails-app"
 set :branch, :master
 server "34.209.102.231", user: "ngochai", roles: %w(web app db)
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') } 
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
-set :assets_roles, [:web, :app] 
 
 
 namespace :puma do
