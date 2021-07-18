da3afd9e592270326bda3258b27656c2c7de95d4
hai.tran <haiit0905@gmail.com>
2021-07-18 15:38:52 +0700

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ set :deploy_to, "/home/ngochai/projects/rails-app"
 set :branch, :master
 server "34.209.102.231", user: "ngochai", roles: %w(web app db)
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_rackup, -> { File.join(current_path, 'config.ru') } 
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
